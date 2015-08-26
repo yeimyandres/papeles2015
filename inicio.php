6e5661e8e26a09092584781e139f9d4b3d69178c
Andrés Arteaga <yeimyandres@gmail.com>
2015-08-26 11:21:33 -0500

    Iniciales

---
+++ inicio.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+
+<html lang="es">
+
+	<head>
+		<title>WorkPapers2015</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+		<link rel="stylesheet" href="./css/estilos.css">
+		<link rel="shortcut icon" href="./img/favicon.ico" />
+	</head>
+
+	<body>
+
+		<header>
+			<h1>Work Papers 2015</h1>
+			<figure>
+				<a href="">
+					<img src="" alt="">
+				</a>
+			</figure>
+		</header>
+
+		<nav>
+			<ul>
+				<li><a href="./secciones/asignaciones">Asignaciones SICA</a></li>
+				<li><a href="">Control Procedimientos</a></li>
+				<li><a href="">Desarrollo Auditoría</a></li>
+				<li><a href="">Anexos Papeles</a></li>
+				<li><a href="">Conclusiones Procedimientos</a></li>
+				<li><a href="">Traslados hallazgos</a></li>
+			</ul>
+		</nav>
+
+		<section class="inicio">
+			<article>
+				<h2>Web App Work Papers</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+					Sed dolor ipsa totam accusantium nulla ad dolorum? Voluptate natus
+					saepe itaque maxime consequatur libero excepturi voluptas quisquam,
+					odit, voluptatum tenetur incidunt!</p>
+			</article>
+		</section>
+
+		<footer>
+			<p class="piedepagina">2015</p>
+			<p class="piedepagina">Yeimy Andrés Arteaga Guerrón</p>
+			<p class="piedepagina">Gerencia Departamental Colegiada Valle del Cauca</p>
+			<p class="piedepagina">Contraloría General de la República</p>
+		</footer>
+
+	</body>
+
+</html>
---
+++ php/asignaciones.php
@@ -0,0 +1,11 @@
+<h2>Ingreso de Asignaciones SICA</h2>
+<form action="">
+	<label for="txtnumat">numero de AT</label>
+	<input type="txtnumat" />
+	<label for="txtasuntoat">Ente o Asunto AT</label>
+	<input type="txtasuntoat" />
+	<label for="txtdescat">Descripción AT SICA</label>
+	<input type="txtdescat" />
+	<label for="txtvigat">Vigencia AT SICA</label>
+	<input type="txtvigat" />
+</form>
